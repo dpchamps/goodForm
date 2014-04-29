0edeef98ea99f7f1780dc882bddc9ef169f8d394
David Campion <gasolinewaltz@gmail.com>
2014-04-28 23:33:20 -0700

    Test form set up.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,55 @@
 /* goodForm main */
 
+var doc = root.document;
+
 // Base function.
-var goodForm = function() {
-  // Add functionality here.
-  return true;
+var goodForm = function(form, options) {
+    function extend( a, b ) {
+        for( var key in b ) {
+            if( b.hasOwnProperty( key ) ) {
+                a[key] = b[key];
+            }
+        }
+        return a;
+    }
+    var default_options = {
+        honeypot : true
+    };
+
+    _init();
+
+    var _init = function(){
+        extend(default_options, options);
+        if(default_options.honeypot){
+            _honeyPot();
+        }
+    };
+
+    var _honeyPot = function(){
+        var input1 = doc.createElement('input'),
+            input2 = doc.createElement('input'),
+            median = Math.floor(form.childNodes.length / 2);
+
+        input1.placeholder("Do Not Fill");
+        input2.placeholder("Do Not Fill");
+
+        input1.style.display = "none";
+        input2.style.display = "none";
+
+        input1.id = "_honey1";
+        input2.id = "_honey2";
+
+        form.appendChild(input2);
+        form.insertBefore(input1, form.childNodes[median]);
+    };
+
+    return {
+        addMask : function(id, o){
+            //
+        }
+
+    }
+
 };
 
-
-// Version.
-goodForm.VERSION = '0.0.0';
-
-
-// Export to the root, which is probably `window`.
 root.goodForm = goodForm;
